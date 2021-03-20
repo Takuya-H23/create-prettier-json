cb2ead18bdd5d6e921bb62facd72b32bddec86d7
Takuya Hirata <takuyahirata4@gmail.com>
2021-03-20 09:51:49 -0400

    feat: init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const path = require("path");
+const prettier = require("./config/prettier.json");
+
+fs.writeFileSync(
+  path.join(process.cwd(), "/prettier.json"),
+  JSON.stringify(prettier)
+);
